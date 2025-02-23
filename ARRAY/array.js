da2310bdade59f6a58596bac572bfc6abfc0b07b
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-02-23 11:41:07 -0300

    Update array.js

--- ARRAY/array.js
+++ ARRAY/array.js
@@ -10,3 +10,6 @@ aprovados.push('Paulo', 'Jeandra') // método PUSH adiciona um elemento no array
 console.log(aprovados)
 console.log(aprovados[3])
 console.log(aprovados[4])
+aprovados.sort() // o método SORT altera o array
+console.log('----------------------------------------')
+console.log(aprovados)
