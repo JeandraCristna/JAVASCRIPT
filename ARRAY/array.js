483567451d60b4f1f18928dac02d7f2638fc1381
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-02-23 11:48:46 -0300

    Update array.js

--- ARRAY/array.js
+++ ARRAY/array.js
@@ -1,15 +1,16 @@
 // array é um objeto no JS
 
-aprovados = ['Bia', 'Luis', 'Ana'] // criando um array
+aprovados = ['Hugo', 'Jadna', 'Karina'] // criando um array
 // acessando elementos do array
 console.log(aprovados[0])
 console.log(aprovados[1])
 console.log(aprovados[2])
-
-aprovados.push('Paulo', 'Jeandra') // método PUSH adiciona um elemento no array
+console.log("----------------------------------------")
+aprovados.push('Alex', 'Jeandra') // método PUSH adiciona um elemento no array
 console.log(aprovados)
 console.log(aprovados[3])
 console.log(aprovados[4])
+
 aprovados.sort() // o método SORT altera o array
 console.log('----------------------------------------')
 console.log(aprovados)
