861f252687f1b990ef7f18eaa5645bb8579cc934
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-02-27 14:38:44 -0300

    Update Metodos_Array.js

--- ARRAY/Metodos_Array.js
+++ ARRAY/Metodos_Array.js
@@ -14,4 +14,5 @@ console.log(frutas)
 frutas.unshift('limão') // o método UNSHIFT adiciona um elemento no primeiro indice(0) do array
 console.log(frutas)
 
-frutas.splice(2, 0, 'maçã', 'amora') // adicinando elementos com o método SPLICE 
+frutas.splice(2, 0, 'maçã', 'amora') // adicionando elementos com o método SPLICE
+console.log(frutas) 
